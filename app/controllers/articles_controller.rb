b16de8c087fdf0e0a51945243d14fbcc5a5fbec9
Akshar Jamgaonkar <akshar.jamgaonkar@ordercup.com>
2016-10-01 15:01:33 +0530

    styles messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article Succesfully Created!!!"
+            flash[:success] = "Article Succesfully Created!!!"
             redirect_to article_path(@article)
         else
             render :new
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
 
     def update
        if @article.update(article_params)
-           flash[:notice] = "Article Succesfully Updated!!!"
+           flash[:success] = "Article Succesfully Updated!!!"
            redirect_to article_path(@article)
        else
            render :edit
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
-        flash[:notice] = "Article Succesfully Destroyed!!!"
+        flash[:danger] = "Article Succesfully Destroyed!!!"
         redirect_to articles_path(@article)
     end
 
