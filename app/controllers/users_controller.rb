92b04dcd78437e7b8fd405fa2de9a1cab582616f
Akshar Jamgaonkar <akshar.jamgaonkar@ordercup.com>
2016-10-06 01:19:44 +0530

    user profile page styling, inetegrate paging for all listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.paginate(page: params[:page],per_page:1)
+    end
+
     def new
         @user = User.new
     end
@@ -28,6 +32,10 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page],per_page:2)
+    end
 
     private
     def user_params
