c7b1fe67211b374e7b017358e3985f586aa8787e
Akshar Jamgaonkar <akshar.jamgaonkar@ordercup.com>
2016-10-16 16:02:23 +0530

    TDD for categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ class CategoriesControllerTest < ActionController::TestCase
     
     def setup
         @category = Category.create(name: "Sports")
+        @user = User.create(username:"jutin",password:"password",email:"friendjustin@myfriend.com",admin:true)
     end
     
     test "should get categorioes index" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
     end
 
     test "should get new" do
+        session[:user_id] = @user.id
         get :new
         assert_response :success
     end
@@ -19,5 +21,13 @@ class CategoriesControllerTest < ActionController::TestCase
         get(:show,{:id=>@category.id})
         assert_response :success
     end
+    
+    test "admin user to only create category" do 
+        assert_no_difference 'Category.count' do
+            post :create, category: {name: "sports"}
+        end
+        assert_redirected_to categories_path
+    end
+    
 
 end
